f6fd59dfba4856ced6f0851bce60b595911dd9b5
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-26 19:42:07 +0900

    ✨. タグ詳細表示機能作成

--- brewery-memo-app/app/Tag.php
+++ brewery-memo-app/app/Tag.php
@@ -3,10 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     protected $fillable = [
         'name',
     ];
+
+    public function breweries(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Brewery')->withTimestamps();
+    }
 }
