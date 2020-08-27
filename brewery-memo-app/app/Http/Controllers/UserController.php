5e4db9d5138e316dfa1bcff552141a6e35e3a640
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-27 18:53:14 +0900

    ✨. フォロー・アンフォロー機能

--- brewery-memo-app/app/Http/Controllers/UserController.php
+++ brewery-memo-app/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class UserController extends Controller
 {
@@ -14,5 +15,35 @@ class UserController extends Controller
         return view('users.show', [
             'user' => $user,
         ]);
+    }
+
+
+    public function follow(Request $request, string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        if ($user->id === $request->user()->id)
+        {
+            return abort('404', 'Cannot follow yourself.');
+        }
+
+        $request->user()->followings()->detach($user);
+        $request->user()->followings()->attach($user);
+
+        return ['name' => $name];
+    }
+
+    public function unfollow(Request $request, string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        if ($user->id === $request->user()->id)
+        {
+            return abort('404', 'Cannot follow yourself.');
+        }
+
+        $request->user()->followings()->detach($user);
+
+        return ['name' => $name];
     }
 }
