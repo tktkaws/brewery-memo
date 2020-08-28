c6db47cfdd4e9921a450e9e6166c50d8361ef009
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-28 15:49:28 +0900

    👍. navbar変更 (テーマカラー・レスポンシブ対応)

--- brewery-memo-app/resources/views/nav.blade.php
+++ brewery-memo-app/resources/views/nav.blade.php
@@ -1,51 +1,67 @@
-<nav class="navbar navbar-expand navbar-dark blue-gradient">
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark amber darken-3">
 
-    <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>Memo</a>
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="/"><i class="fas fa-beer mr-1"></i>Brewry Memo</a>
 
-    <ul class="navbar-nav ml-auto">
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+        aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-        @guest
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
-        </li>
-        @endguest
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="basicExampleNav">
 
-        @guest
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('login') }}">ログイン</a>
-        </li>
-        @endguest
+        <!-- Links -->
+        <ul class="navbar-nav mr-auto">
+            @guest
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">ログイン</a>
+            </li>
+            @endguest
 
-        @auth
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('breweries.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a>
-        </li>
-        @endauth
+            @auth
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('breweries.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a>
+            </li>
+            @endauth
 
-        @auth
-        <!-- Dropdown -->
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
-                aria-expanded="false">
-                <i class="fas fa-user-circle"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-                <button class="dropdown-item" type="button"
-                    onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
-                    マイページ
-                </button>
-                <div class="dropdown-divider"></div>
-                <button form="logout-button" class="dropdown-item" type="submit">
-                    ログアウト
-                </button>
-            </div>
-        </li>
-        <form id="logout-button" method="POST" action="{{ route('logout') }}">
-            @csrf
-        </form>
-        <!-- Dropdown -->
-        @endauth
+            @auth
+            <!-- Dropdown -->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-user-circle"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-primary"
+                    aria-labelledby="navbarDropdownMenuLink">
+                    <button class="dropdown-item" type="button"
+                        onclick="location.href='{{ route('users.show', ['name' => Auth::user()->name]) }}'">
+                        マイページ
+                    </button>
+                    <div class="dropdown-divider"></div>
+                    <button form="logout-button" class="dropdown-item" type="submit">
+                        ログアウト
+                    </button>
+                </div>
+            </li>
+            <form id="logout-button" method="POST" action="{{ route('logout') }}">
+                @csrf
+            </form>
+            <!-- Dropdown -->
+            @endauth
 
-    </ul>
+        </ul>
+        <!-- Links -->
+
+        <!-- Search form -->
+        <!-- Search form -->
+    </div>
+    <!-- Collapsible content -->
 
 </nav>
+<!--/.Navbar-->
