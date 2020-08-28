adede18ad296aa5c7320c7097a19079b35a4f6e6
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-28 15:56:38 +0900

    👍. カードの構成を変更

--- brewery-memo-app/resources/views/breweries/card.blade.php
+++ brewery-memo-app/resources/views/breweries/card.blade.php
@@ -1,26 +1,59 @@
 <div class="card mt-3">
+    <br>
+    <div class="card-body pt-0">
+        <h3 class="h4 card-title">
+            <a class="text-dark" href="{{ route('breweries.show', ['brewery' => $brewery]) }}">
+                {{ $brewery->name }}
+            </a>
+        </h3>
+        <div class="card-text">
+            {{ $brewery->body }}
+        </div>
+    </div>
+    @foreach($brewery->tags as $tag)
+    @if($loop->first)
+    <div class="card-body pt-0 pb-4 pl-3">
+        <div class="card-text line-height">
+            @endif
+            <a href="{{ route('tags.show', ['name' => $tag->name]) }}" class="border p-1 mr-1 mt-1 text-muted">
+                {{ $tag->name }}
+
+            </a>
+            @if($loop->last)
+        </div>
+    </div>
+    @endif
+    @endforeach
+
+    <div class="card-body pt-0 pb-2 pl-3">
+        <div class="card-text">
+            <brewery-like :initial-is-liked-by='@json($brewery->isLikedBy(Auth::user()))'
+                :initial-count-likes='@json($brewery->count_likes)' :authorized='@json(Auth::check())'
+                endpoint="{{ route('breweries.like', ['brewery' => $brewery]) }}">
+            </brewery-like>
+        </div>
+    </div>
+
     <div class="card-body d-flex flex-row">
         <a href="{{ route('users.show', ['name' => $brewery->user->name]) }}" class="text-dark">
             <i class="fas fa-user-circle fa-3x mr-1"></i>
         </a>
+
         <div>
             <div class="font-weight-bold">
                 <a href="{{ route('users.show', ['name' => $brewery->user->name]) }}" class="text-dark">
                     {{ $brewery->user->name }}
                 </a>
             </div>
-            <div class="font-weight-lighter">
-                {{ $brewery->created_at->format('Y/m/d H:i') }}
-            </div>
+            <div class="font-weight-lighter">{{ $brewery->created_at->format('Y/m/d H:i') }}</div>
         </div>
+
         @if( Auth::id() === $brewery->user_id )
         <!-- dropdown -->
         <div class="ml-auto card-text">
             <div class="dropdown">
                 <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <button type="button" class="btn btn-link text-muted m-0 p-2">
-                        <i class="fas fa-ellipsis-v"></i>
-                    </button>
+                    <i class="fas fa-ellipsis-v"></i>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right">
                     <a class="dropdown-item" href="{{ route("breweries.edit", ['brewery' => $brewery]) }}">
@@ -61,39 +94,6 @@
         </div>
         <!-- modal -->
         @endif
-
-    </div>
-    <div class="card-body pt-0">
-        <h3 class="h4 card-title">
-            <a class="text-dark" href="{{ route('breweries.show', ['brewery' => $brewery]) }}">
-                {{ $brewery->name }}
-            </a>
-        </h3>
-        <div class="card-text">
-            {{ $brewery->body }}
-        </div>
     </div>
 
-    <div class="card-body pt-0 pb-2 pl-3">
-        <div class="card-text">
-            <brewery-like　 :initial-is-liked-by='@json($brewery->isLikedBy(Auth::user()))'
-                :initial-count-likes='@json($brewery->count_likes)' :authorized='@json(Auth::check())'
-                endpoint="{{ route('breweries.like', ['brewery' => $brewery]) }}">
-            </brewery-like>
-        </div>
-    </div>
-
-    @foreach($brewery->tags as $tag)
-    @if($loop->first)
-    <div class="card-body pt-0 pb-4 pl-3">
-        <div class="card-text line-height">
-            @endif
-            <a href="{{ route('tags.show', ['name' => $tag->name]) }}" class="border p-1 mr-1 mt-1 text-muted">
-                {{ $tag->name }}
-            </a>
-            @if($loop->last)
-        </div>
-    </div>
-    @endif
-    @endforeach
 </div>
